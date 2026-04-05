30d626759140b3ff39dcda56f348b99b0828fe77
casalecchi <felipevilela191@gmail.com>
2026-04-04 21:56:31 -0300

    feat: migrate from HashRouter to BrowserRouter and add 404 fallback for SPA hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './i18n';
 import theme from './theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
